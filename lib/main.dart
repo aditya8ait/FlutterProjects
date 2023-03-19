707a140ca9c8e1d785e86d64d3a00ef7c4322e9c
aditya8ait <adityatiwari_21305@aitpune.edu.in>
2023-03-19 11:05:37 +0530

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:project1/home_page.dart';
+import 'package:project1/pages/login_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      // home: HomePage(),
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/login": (context) => HomePage(),
+      },
       // title: 'Flutter Demo',
       // theme: ThemeData(
       //   // This is the theme of your application.
